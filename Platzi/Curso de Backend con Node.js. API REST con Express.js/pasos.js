899783ff71c740a71892063d61f6852ce25d6f53
Federico Irarrazaval <fedeirar@gmail.com>
2023-02-27 11:05:45 -0300

    entorno de produccion

---
+++ Platzi/Curso de Backend con Node.js. API REST con Express.js/pasos.js
@@ -0,0 +1,29 @@
+/*
+!EXPRESS.JS
+
+!1) ENTORNO DE PRODUCCIÓN:
+
+Primero trabajamos con el entorno utilizando nodemon como servidor de desarrollo.
+
+*1) npm init -y
+*2) .gitignore
+Puedo usar gitignore.io (https://www.toptal.com/developers/gitignore/) Le agregamos node, windows, linux y macOS y luego create
+Te da una configuración de todos los archivos que no deberíamos tener en cuenta en neustro repo. Copiamos y pegamos en .gitignore
+*3) .editorconfig
+Para que todos los desarrolladores tengan una misma configuración de editor.
+*4) .eslintrc.json
+Lo mismo de editorconfig con eslint. Para usar mismo standard
+*5) index.js (crear archivo)
+*6) package.json
+A) En scripts agregamos "dev": "nodemon index.js" para levantar un entorno de desarrollo.
+B) Para producción: "start": "node index.js"
+C) "lint": "eslint" para correr eslint
+*7) agregar dependencias
+A) npm i nodemon eslint eslint-config-prettier eslint-plugin-prettier prettier -D
+El -D es porque son dependencias para modo de desarrollo y no para producción.
+*8) npm run dev
+Corres el entorno de desarrollo.
+*9) npm run start
+Corres el entorno de producción.
+
+*/
