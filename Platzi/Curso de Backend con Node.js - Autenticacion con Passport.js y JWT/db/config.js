1a449987570a9c3f5e42a82c176b0d74f7749cd1
Federico Irarrazaval <fedeirar@gmail.com>
2023-03-05 16:40:03 -0300

    deploy v5

--- Platzi/Curso de Backend con Node.js - Autenticacion con Passport.js y JWT/db/config.js
+++ Platzi/Curso de Backend con Node.js - Autenticacion con Passport.js y JWT/db/config.js
@@ -1,17 +1,26 @@
 const { config } = require('./../config/config');
 
-module.exports = {
-  development: {
-    url: config.dbUrl,
-    dialect: 'postgres',
-  },
-  production: {
-    url: config.dbUrl,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false
-      }
-    }
-  }
-}
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(config.dbUrl, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
+
+module.exports = sequelize;
+
+// module.exports = {
+//   development: {
+//     url: config.dbUrl,
+//     dialect: 'postgres',
+//   },
+//   production: {
+//     url: config.dbUrl,
+//     dialect: 'postgres',
+//     dialectOptions: {
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//     },
+//   },
+// };
