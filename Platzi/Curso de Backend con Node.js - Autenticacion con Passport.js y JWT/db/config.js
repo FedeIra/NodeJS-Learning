41a62cc4e3d693b9c8f8de44ee69b625707ea423
Federico Irarrazaval <fedeirar@gmail.com>
2023-03-05 16:58:49 -0300

    deploy v6

--- Platzi/Curso de Backend con Node.js - Autenticacion con Passport.js y JWT/db/config.js
+++ Platzi/Curso de Backend con Node.js - Autenticacion con Passport.js y JWT/db/config.js
@@ -1,14 +1,40 @@
-const { config } = require('./../config/config');
-
 const { Sequelize } = require('sequelize');
+// const { config } = require('./../config/config');
 
-const sequelize = new Sequelize(config.dbUrl, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+require('dotenv').config();
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+
+let sequelize =
+  process.env.NODE_ENV === 'production'
+    ? new Sequelize({
+        database: DB_NAME,
+        dialect: 'postgres',
+        host: DB_HOST,
+        port: DB_PORT,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
+    : new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/my_store`,
+        { logging: false, native: false }
+      );
 
 module.exports = sequelize;
 
+// const { config } = require('./../config/config');
 // module.exports = {
 //   development: {
 //     url: config.dbUrl,
