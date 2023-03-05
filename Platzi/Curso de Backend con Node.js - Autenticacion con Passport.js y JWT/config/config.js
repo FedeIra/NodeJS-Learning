b93d312aed5a977fe09fdb8031c57204f7c43bad
Federico Irarrazaval <fedeirar@gmail.com>
2023-03-05 13:49:16 -0300

    deploy v2 with DB

--- Platzi/Curso de Backend con Node.js - Autenticacion con Passport.js y JWT/config/config.js
+++ Platzi/Curso de Backend con Node.js - Autenticacion con Passport.js y JWT/config/config.js
@@ -4,7 +4,7 @@ const config = {
   env: process.env.NODE_ENV || 'dev',
   isProd: process.env.NODE_ENV === 'production',
   port: process.env.PORT || 3000,
-  dbUrl: process.env.DATABASE_URL,
+  dbUrl: process.env.DB_DEPLY,
   apiKey: process.env.API_KEY,
   jwtSecret: process.env.JWT_SECRET,
   jwtSecretRecovery: process.env.JWT_SECRET_RECOVERY,
