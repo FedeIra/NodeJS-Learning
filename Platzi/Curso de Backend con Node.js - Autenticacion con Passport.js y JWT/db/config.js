c313a5be375e38be23f815695d9166b38a2071bf
Federico Irarrazaval <fedeirar@gmail.com>
2023-03-05 17:40:23 -0300

    deploy final

--- Platzi/Curso de Backend con Node.js - Autenticacion con Passport.js y JWT/db/config.js
+++ Platzi/Curso de Backend con Node.js - Autenticacion con Passport.js y JWT/db/config.js
@@ -1,52 +1,17 @@
-const { Sequelize } = require('sequelize');
-// const { config } = require('./../config/config');
+const { config } = require('./../config/config');
 
-require('dotenv').config();
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
-
-let sequelize =
-  process.env.NODE_ENV === 'production'
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: 'postgres',
-        host: DB_HOST,
-        port: DB_PORT,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
-    : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/my_store`,
-        { logging: false, native: false }
-      );
-
-module.exports = sequelize;
-
-// const { config } = require('./../config/config');
-// module.exports = {
-//   development: {
-//     url: config.dbUrl,
-//     dialect: 'postgres',
-//   },
-//   production: {
-//     url: config.dbUrl,
-//     dialect: 'postgres',
-//     dialectOptions: {
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
-//     },
-//   },
-// };
+module.exports = {
+  development: {
+    url: config.dbUrl,
+    dialect: 'postgres',
+  },
+  production: {
+    url: config.dbUrl,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
--- Platzi/Curso de Backend con Node.js - Autenticacion con Passport.js y JWT/index.js
+++ Platzi/Curso de Backend con Node.js - Autenticacion con Passport.js y JWT/index.js
@@ -17,8 +17,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// const whitelist = ['http://localhost:8080', 'https://myapp.co'];
-const whitelist = ['http://localhost:8080', '*']; // deploy
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
